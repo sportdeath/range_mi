17013d0eabebe8b593f47715efbf6f561758f8d5
tfh <tfh@mit.edu>
2019-04-12 15:43:19 -0400

    Added timing

--- test/mutual_information.cpp
+++ test/mutual_information.cpp
@@ -1,6 +1,7 @@
 #include <random>
 #include <vector>
 #include <iostream>
+#include <chrono>
 
 #include <wandering_robot/occupancy_state.hpp>
 #include <wandering_robot/mutual_information.hpp>
@@ -8,8 +9,8 @@
 using namespace wandering_robot;
 
 // Initialize constants
-double poisson_rate = 1.;
-int num_iterations = 100;
+double poisson_rate = 0.1234;
+int num_iterations = 1000;
 size_t num_cells = 1000;
 double p_occupied = 0.05;
 double p_unknown = 0.5;
@@ -52,6 +53,11 @@ int main() {
   std::vector<double> p_not_measured(num_cells);
   std::vector<double> mi_1d(num_cells), mi_2d(num_cells);
 
+  // Initialize timing
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  double time_1d_joint = 0, time_1d_individual = 0;
+  double time_2d_joint = 0, time_2d_individual = 0;
+
   // Initialize the mutual information
   MutualInformation mi(poisson_rate);
 
@@ -60,13 +66,32 @@ int main() {
     random_cells(states, widths, p_not_measured);
 
     // Compute the mutual information all at once
+
+    // In 1 dimension ...
+    start = std::chrono::system_clock::now();
     mi.d1(states.data(), widths.data(), p_not_measured.data(), num_cells, mi_1d.data());
+    end = std::chrono::system_clock::now();
+    time_1d_joint += std::chrono::duration<double>(end - start).count();
+
+    // ... and in 2 dimensions.
+    start = std::chrono::system_clock::now();
     mi.d2(states.data(), widths.data(), p_not_measured.data(), num_cells, mi_2d.data());
+    end = std::chrono::system_clock::now();
+    time_2d_joint += std::chrono::duration<double>(end - start).count();
 
     // Compute the mutual information one at a time
     for (unsigned int j = 0; j < num_cells; j++) {
+      // In 1 dimension ...
+      start = std::chrono::system_clock::now();
       double mi_1d_cell = mi.d1(states.data() + j, widths.data() + j, p_not_measured.data() + j, num_cells);
+      end = std::chrono::system_clock::now();
+      time_1d_individual += std::chrono::duration<double>(end - start).count();
+
+      // ... and in 2 dimensions.
+      start = std::chrono::system_clock::now();
       double mi_2d_cell = mi.d2(states.data() + j, widths.data() + j, p_not_measured.data() + j, num_cells);
+      end = std::chrono::system_clock::now();
+      time_2d_individual += std::chrono::duration<double>(end - start).count();
 
       double error_1d = std::abs(mi_1d_cell - mi_1d[j]);
       if (error_1d > 0.000001) {
@@ -84,6 +109,11 @@ int main() {
       }
     }
   }
+
+  std::cout << "1D individual time: " << time_1d_individual  << " seconds" << std::endl;
+  std::cout << "1D joint time: " << time_1d_joint  << " seconds" << std::endl;
+  std::cout << "2D individual time: " << time_2d_individual  << " seconds" << std::endl;
+  std::cout << "2D joint time: " << time_2d_joint  << " seconds" << std::endl;
 
   return 0;
 }
